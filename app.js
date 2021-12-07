3aef1774ae0cafbe5e94ac8e687d7dd17011c62d
AgathePons <ponsagathe37@gmail.com>
2021-12-07 18:31:14 +0100

    setting homepage route, public folder, ejs render : index page with css available

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = require('./app/router');
 const server = express();
 
+server.set('view engine', 'ejs');
+server.set('views', './app/views');
+
+server.use(express.static('public'));
+
+server.use(router);
+
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`linstening on ${PORT}`);
---
+++ app/data/marioKartCharacters.js
@@ -0,0 +1,43 @@
+const characters = [
+  {
+    idName: 'mario',
+    name: 'Mario',
+    category: 'medium',
+    subcategory: 'medium-4',
+    picture: 'https://www.eclypsia.com/content/Nintendo/MarioKart8Deluxe/Personnages/Mario.png',
+    speed: 3.75,
+    acceleration: 2.5,
+    weight: 3.75,
+    maniability: 3.25,
+    grip: 3.75,
+    minitrbo: 2.25
+  },
+  {
+    idName: 'link',
+    name: 'Link',
+    category: 'heavy',
+    subcategory: 'heavy-1',
+    picture: 'https://www.eclypsia.com/content/Nintendo/MarioKart8Deluxe/Personnages/Link.png',
+    speed: 4.25,
+    acceleration: 2.25,
+    weight: 4.25,
+    maniability: 2.75,
+    grip: 3.5,
+    minitrbo: 2
+  },
+  {
+    idName: 'roiBoo',
+    name: 'Roi Boo',
+    category: 'heavy',
+    subcategory: 'heavy-2',
+    picture: 'https://www.eclypsia.com/content/Nintendo/MarioKart8Deluxe/Personnages/RoiBoo.png',
+    speed: 4.25,
+    acceleration: 2,
+    weight: 4.75,
+    maniability: 2.75,
+    grip: 3.25,
+    minitrbo: 1.75
+  }
+];
+
+module.exports = characters;
